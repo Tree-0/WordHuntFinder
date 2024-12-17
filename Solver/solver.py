a0b4cdb8e7c64032dc3a7908d3073d02bc9c20a0
Tree-0 <117487029+Tree-0@users.noreply.github.com>
2024-12-17 01:27:58 -0600

    anagrams solver complete

--- Solver/solver.py
+++ Solver/solver.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from collections import Counter
 
 # Add the parent directory of Solver and Trie to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -10,18 +11,19 @@ class Solver:
     def __init__(self, letters:str, mode:str, board_size:int):
         self.board = None
         self.board_size = board_size
-        letters = letters.upper()
+        self.letters = letters.upper()
+        self.mode = mode
 
         if mode == 'wordhunt':
             # square matrix
             self.board = [['']*board_size for i in range(board_size)] 
-            if not len(letters) == board_size ** 2:
+            if not len(self.letters) == board_size ** 2:
                 raise ValueError('invalid amount of letters given. Must fill square board exactly.')
             
             row = 0
             col = 0
             # fill out board with given letters
-            for l in letters:
+            for l in self.letters:
                 self.board[row][col] = l
                 col += 1
                 if col == board_size: # wrap to next row
@@ -31,7 +33,7 @@ class Solver:
         elif mode == 'anagrams':
             # don't need board, just letters
             self.board = None
-            if not len(letters) == board_size:
+            if not len(self.letters) == board_size:
                 raise ValueError('invalid amount of letters given. Must match board size.')
             
         else:
@@ -43,6 +45,8 @@ class Solver:
         Takes a trie of accepted words to compare to.
         Valid words must be at least length 3.
         '''
+
+        assert self.mode == 'wordhunt'
 
         visited = set() # the letters used so far in a word
         found_words = set() # valid words found in board
@@ -68,7 +72,6 @@ class Solver:
             if depth >= 3 and valid_words.contains_word(curr_word):
                 #print('checking string', curr_word)
                 print('valid word:', curr_word)
-                nonlocal found_words
                 found_words.add(curr_word)
     
             find_words(x+1,y, curr_word, depth+1) # down
@@ -87,8 +90,52 @@ class Solver:
         for i in range(len(self.board)):
             for j in range(len(self.board[0])):
                 visited.clear()
-                print('starting from letter: ', self.board[i][j])
+                print(f'({i},{j}) - starting from letter: ', self.board[i][j])
                 find_words(i,j,'',depth=1)
         
         return found_words
+
+    def solve_anagrams(self, valid_words:Trie) -> set:
+        ''' 
+        Solve the anagrams given to the solver. 
+        Takes a trie of accepted words to compare to.
+        Valid words must be at least length 3.
+        '''
+
+        assert self.mode == 'anagrams'
+
+        unused = Counter(self.letters) # the letters to be used in a word
+        found_words = set() # valid words found in board
+
+        def permute(curr_word:str, depth:int):
+            # print(curr_word)  
+            
+            if not valid_words.contains_prefix(curr_word):
+                # print(f'no valid prefix: ', curr_word)
+                return # no possible word could be made
+
+            #print('string: ', curr_word, 'depth: ', depth)
+            if depth >= 3 and valid_words.contains_word(curr_word):
+                print('valid word:', curr_word)
+                found_words.add(curr_word)
+
+            for let in list(unused.keys()):
+                # check if any instances of this letter can be used
+                if unused[let] > 0:
+                    unused[let] -= 1 # use one letter
+
+                    #print(f'removed {let}. Remaining: {unused}')
+                    permute(curr_word+let, depth+1)
+                    
+                    unused[let] += 1 # restore letter for other recursive branches
+                    #print(f'added {let}. Remaining: {unused}')
+
+        permute('', 0)
+        return found_words
+
+            
+
+
+
+
 
--- Solver/solver_unit_test.py
+++ Solver/solver_unit_test.py
@@ -44,7 +44,7 @@ def test_word_hunt_small_board():
     print(f'dictionary size: {valid_words.word_count} words, {valid_words.letter_count} letters.')
 
     now = time.time()
-    with open('output_small.txt', 'w') as file:
+    with open('wordhunt_small.txt', 'w') as file:
         with redirect_stdout(file):
             found_words = small_solver.solve_word_hunt(valid_words)
 
@@ -52,7 +52,6 @@ def test_word_hunt_small_board():
 
     print(f'solving board took {int(then-now)} seconds.')
 
-    assert found_words.pop()
     assert len(found_words) > 0
     print(f'{len(found_words)} words found.')
 
@@ -70,7 +69,7 @@ def test_word_hunt_medium_board():
     print(f'dictionary size: {valid_words.word_count} words, {valid_words.letter_count} letters.')
 
     now = time.time()
-    with open('output_medium.txt', 'w') as file:
+    with open('wordhunt_medium.txt', 'w') as file:
         with redirect_stdout(file):
             found_words = solver.solve_word_hunt(valid_words)
 
@@ -78,7 +77,6 @@ def test_word_hunt_medium_board():
 
     print(f'solving board took {int(then-now)} seconds.')
 
-    assert found_words.pop()
     assert len(found_words) > 0
     print(f'{len(found_words)} words found.')
 
@@ -94,7 +92,7 @@ def test_word_hunt_large_board():
     print(f'deserialization took {int(then-now)} seconds.')
     now = time.time()
 
-    with open('output_large.txt', 'w') as file:
+    with open('wordhunt_large.txt', 'w') as file:
         with redirect_stdout(file):
             found_words = large_solver.solve_word_hunt(valid_words)
 
@@ -102,6 +100,41 @@ def test_word_hunt_large_board():
 
     print(f'solving board took {int(then-now)} seconds.')
 
-    assert found_words.pop()
+    assert len(found_words) > 0
+    print(f'{len(found_words)} words found.')
+
+def test_anagrams_small():
+    small_solver = Solver('seats', 'anagrams', 5)
+    
+    now = time.time()
+
+    valid_words = Trie()
+    valid_words.deserialize_from_file('trie_data.pickle', format='pickle')
+
+    then = time.time()
+    print(f'deserialization took {int(then-now)} seconds.')
+    now = time.time()
+
+    with open('anagrams_small.txt', 'w') as file:
+        with redirect_stdout(file):
+            found_words = small_solver.solve_anagrams(valid_words)
+
+    then = time.time()
+
+    print(f'solving anagrams took {int(then-now)} seconds.')
+
+    assert len(found_words) > 0
+    print(f'{len(found_words)} words found.')
+
+def test_anagrams_medium():
+    medium_solver = Solver('seated', 'anagrams', 6)
+
+    valid_words = Trie()
+    valid_words.deserialize_from_file('trie_data.json', format='json')
+
+    with open('anagrams_medium.txt', 'w') as file:
+        with redirect_stdout(file):
+            found_words = medium_solver.solve_anagrams(valid_words)
+
     assert len(found_words) > 0
     print(f'{len(found_words)} words found.')
